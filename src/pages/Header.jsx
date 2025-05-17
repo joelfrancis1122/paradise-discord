85ac8195b958238b9c8fdae3c87397da68939384
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-05-17 13:31:56 +0530

    udpated cart icon

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -37,7 +37,7 @@ export default function Header({ cart, removeFromCart, updateCartQuantity, clear
                         </span>
                     )}
                     &nbsp;
-                    <p>cart</p>
+                    <p>Cart</p>
                 </button>
             </header>
 
